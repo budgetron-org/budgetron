2ae8025614759c32440601fe7ff91c04e1c72051
Raghavan Renganathan <28684823+navahg@users.noreply.github.com>
2025-06-16 14:50:46 -0400

    version 0.1.1

--- src/data/changelog.ts
+++ src/data/changelog.ts
@@ -22,6 +22,26 @@ type Changelog = {
 
 const CHANGELOGS: Changelog[] = [
   {
+    version: '0.1.1',
+    date: '2025-06-16',
+    description: 'Fixes to the transaction form and table',
+    changes: [],
+    features: [],
+    fixes: [
+      {
+        title: 'Fixed category picker in transaction form',
+        description:
+          'Before this fix, the category picker would not update when the type was changed. Now it does.',
+      },
+      {
+        title: 'Fixed transaction table key conflicts',
+        description:
+          'Before this fix, multiple transactions without an account assigned on the same date would cause key conflicts. Now it does not.',
+      },
+    ],
+    breakingChanges: [],
+  },
+  {
     version: '0.1.0',
     date: '2025-06-16',
     description: 'Initial release',
