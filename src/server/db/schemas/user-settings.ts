14d45c010355d5b4cda099679f41df10e67085c8
Raghavan Renganathan <28684823+navahg@users.noreply.github.com>
2025-07-06 10:27:33 -0400

    fix user settings table

--- src/server/db/schemas/user-settings.ts
+++ src/server/db/schemas/user-settings.ts
@@ -1,18 +1,22 @@
 import { relations } from 'drizzle-orm'
-import { pgTable, text, uuid } from 'drizzle-orm/pg-core'
+import { pgTable, text, unique, uuid } from 'drizzle-orm/pg-core'
 
 import { createdAt, id, updatedAt } from '../utils'
 import { UserTable } from './user'
 
-const UserSettingsTable = pgTable('user_settings', {
-  id,
-  userId: uuid()
-    .references(() => UserTable.id, { onDelete: 'cascade' })
-    .notNull(),
-  currency: text().notNull().default('USD'),
-  createdAt,
-  updatedAt,
-})
+const UserSettingsTable = pgTable(
+  'user_settings',
+  {
+    id,
+    userId: uuid()
+      .references(() => UserTable.id, { onDelete: 'cascade' })
+      .notNull(),
+    currency: text().notNull().default('USD'),
+    createdAt,
+    updatedAt,
+  },
+  (t) => [unique().on(t.userId)],
+)
 
 const UserSettingsRelations = relations(UserSettingsTable, ({ one }) => ({
   user: one(UserTable, {
